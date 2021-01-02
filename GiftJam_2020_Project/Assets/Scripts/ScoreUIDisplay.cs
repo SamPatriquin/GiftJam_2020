ef82af7b63eb922f6209e0ad0587d33845d96fad
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2021-01-01 23:55:36 -0500

    Added score UI animation

--- GiftJam_2020_Project/Assets/Scripts/ScoreUIDisplay.cs
+++ GiftJam_2020_Project/Assets/Scripts/ScoreUIDisplay.cs
@@ -7,14 +7,18 @@ public class ScoreUIDisplay : MonoBehaviour {
     [SerializeField] Score score;
 
     private Text text;
+    private Animator pearlAnimator;
+    private int animatorPearlGrow = Animator.StringToHash("grow");
 
     private void Awake() {
         text = this.GetComponentInChildren<Text>();
+        pearlAnimator = this.GetComponentInChildren<Animator>(); //BAD, change this 
         text.text = "0";
         score.OnScoreIncrease += IncreaseScore;
     }
 
     private void IncreaseScore(int score) {
         text.text = score.ToString();
+        pearlAnimator.SetTrigger(animatorPearlGrow);
     }
 }
